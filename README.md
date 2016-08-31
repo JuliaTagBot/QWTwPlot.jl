8e3b5225ae8450405a199145b3cf3d78977da0a0
IGOR SANDLER <igor271828@gmail.com>
2016-08-31 12:22:22 +0300

    example and description changed

--- README.md
+++ README.md
@@ -2,26 +2,22 @@
 
 This is another 2D plotting tool for Julia language.  It is based on (my) `qwtw` `"C"` library, which is based on `qwt` library which is based on `QT` library.
 
-Current version works only for `Windows7 x64`.
-In short, how to install it (for Windows7 x64):
+Current version should work both for `Windows` and for `Linux`.
+
+how to install it
 ----------------------------
-* install `qwtw` library using an installer from
-		https://github.com/ig-or/qwtw/releases/download/v1.0.1/qwtwsetup.exe
-* in Julia command prompt, run 		`Pkg.clone("https://github.com/ig-or/qwtwplot")`
+* install `qwtw` library using instructions from here
+		https://github.com/ig-or/qwtw
+* in Julia command prompt, run 	`Pkg.clone("https://github.com/ig-or/qwtwplot")`
 * look at usage example https://github.com/ig-or/qwtwplot/blob/master/src/qwexample.jl
 * and let the force be with you
 
 Another description
 ----------------------------
 You can see how it looks like here:
-		https://github.com/ig-or/qwtw - on qwtw library page. There are a number of pictures in the description.
+		https://github.com/ig-or/qwtw - on qwtw library page. There are a number of pictures in this description.
 
-Current version works only for `Windows7 x64`, but it should not be difficult to make everything work for x32 Windows and for Linux. Actually I'm using all this both from Windows and from Fedora Linux.
-
-In orider to use this package, you'll have to have `qwtw` library "installed". For `Windows7 x64` I have an "installer"
-		https://github.com/ig-or/qwtw/releases/download/v1.0.1/qwtwsetup.exe
-
-For Linux you'll have to build `qwtw` library by yourself. It depends on a number of other libraries, so it can be a bit tricky (but for `Fedora Linux` instructions from `qwtw` project page works I should be able to create at least an RPM when I will have more time).
+In order to use this package, you'll have to have `qwtw` library "installed". For `Windows7 x64` and `Fedora-24-x64 Linux`  I have installation packages  https://github.com/ig-or/qwtw/releases
 
 
 [![Build Status](https://travis-ci.org/ig-or/qwtwplot.jl.svg?branch=master)](https://travis-ci.org/ig-or/qwtwplot.jl)
--- src/qwexample.jl
+++ src/qwexample.jl
@@ -6,15 +6,15 @@ using qwtwplot
 qwtwStart() # start 'qwtwc' library
 
 # draw thin blue 'sinus':
-tMax = 10.
-t = Array(linspace(0.,tMax, 10000));
+tMax = 10. # let it be `maximum time`
+t = Array(linspace(0.,tMax, 10000)); # our `time info`
 n = length(t);
-y = sin(t);
+y = sin(t);     # we will draw this signal
 qfigure(1); # "1" the number of the first plot window
  # parameters: 'x' and 'y' data vectors, then 'name of this line', then 'style description', then 'line width'
 qplot(t, y, "blue line", "-b", 1)
 
-# add green thick another sinus:
+# add green thick another sinus on the same plot:
 y = sin(t .* 4.) + cos(t * 0.8);
 qplot(t, y, "thick green line", "-g", 4) #'x' and 'y' data vectors, then 'name of this line', then 'style description', then 'line width'
 qtitle("first plot window") # add a title for the first plot
@@ -22,29 +22,37 @@ qxlabel("time (in seconds)") # put a label on X axis
 qylabel("happiness") # put a label on Y axis
 
 #= By default, "ZOOM" mode is active. This means that
-	now you can select some part of the plot with left mouse button.
-	Right mouse button will return you back to the previous state.
-	And try mouse wheel also.
+	now you can select some part of the plot with left mouse button
+	(press left mouse button, select what you need on a plot,
+	release left mouse button).
+	Right mouse button will return you back to the previous `ZOOM` state.
+	And try how mouse wheel is working in this mode also.
 
-	Button with a "hand" - it's a "pan mode".  It will shift all the plots, but not change scale.
-	"disk" button - allow you to save image to a file
+	Button with a "hand" - it's a "pan mode".  It will shift all the picture,
+	but it will not change scale.
+	"disk" button - allow you to save image to a (png) file
 	"[]" buttom means "make square axis" i.e. make equal scale for X and Y axis.
+	this `[]` button is useful when you draw something like a `top view plot`
 
 	Real magic is in left and right buttons; see below about it.
 
 =#
 
-# create another plot, with high frequency signal
+# create another plot, with high frequency signal and noise
 noise = rand(n);
 y = sin(t * 100.) + noise;
 qfigure(2)
 qplot(t, y, "sinus + noise", "-m", 2)
 qtitle("frequency test")
 
-# what is the frequency of our signal from last window?
-# pless "f" button, on new small window select "sinus + noise",
-# select "4" from combo box and close this small window
-# after this "frequency" plot will be created
+#= what is the frequency of our signal from last window?
+ pless "f" button, another small window will appear;
+ then on new small window select "sinus + noise",
+ select "4" from combo box and close this small window (with Alt-F4 for example)
+
+ after this, "frequency" plot will be created,
+ actually, it is called "Power Spectral Dencity plot"
+=#
 
 # add another line to the first plot:
 t1 = Array(linspace(0.,10., 5))
@@ -54,18 +62,27 @@ qfigure(1); # switch back to the first plot
 # if we do not need the lines, only symbols:
 qplot1(t1, y1, "points and line", " eb",  30)
 
+#= the parameter #4 in qplot or qplot1 functions is a "line style"
+	it is a string consisting from 1 or 2 or 3 characters
+	the last symbol is a color description;
+	first is always a line style:
+	middle is symbol type
+
+	for additional info about this line style,
+	see info here:
+	https://raw.githubusercontent.com/wiki/ig-or/qwtwplot/line-style.md
+=#
+
  # parameters: 'x' and 'y' data vectors, then 'name of this line',
  # then 'style description', then 'line width', and "symbol size"
 qplot(t1, y1, "points and line", "-tm", 2, 10)
 
 #= now, try to use an arrow burtton: it will draw a marker on all the plots.
+	Press "Arrow" button, then left mouse button somewhere inside the plot.
 
 	Next, select some region on one of the plot with ZOOM tool;  and press right "clip" button.
-	All the plots will zoom to the same region.
-	There is a bug with "top view" plots, but it can be fixed.
-
+	All the plots will (try to) zoom to the same region.
 =#
-
 
 # create a circle on another plot window:
 x = sin(t*2.*pi/tMax); y = cos(t*2.*pi/tMax);
@@ -77,20 +94,22 @@ qtitle("circle")
 
 # try to use left button ("ARROW") on a circle plot - it also works
 
-# draw one more circle:
+# draw one more circle on the same plot:
 t1 = Array(linspace(0.,2. * pi, 8));
 x1 = 0.5*sin(t1);
 y1 = 0.5*cos(t1);
 qplot2p(x1, y1, "circle #2", " ec", 20, t1)
 
-# show "main window":
+#= show "main window" in order to control all other windows:
+it will allow you to switch between all other plot windows
+=#
 qsmw()
 
-# now lets try to draw a map
-mwn = 4 # for example, we have only 4 points
+# now lets try to draw a map;
+mwn = 4 # for this small example, we have only 4 points
 north = [55.688713, 55.698713, 55.678713, 55.60]; # coords in degrees
 east = [37.901073, 37.911073, 37.905073, 37.9]; # coords in degrees
-t4 = Array(linspace(0.,tMax, 4)); # create corresponding time info (for magic markers)
+t4 = Array(linspace(0.,tMax, 4)); # create corresponding time info (for our cool magic markers)
 
 qfmap(5)
 qplot2p(east, north, "trajectory #1", "-rb",  20, t4);
@@ -103,7 +122,6 @@ qfmap(6)
 qplot2(east1, north1, "trajectory #2", "-rb",  2, t4);
 qplot2(east1, north1, "points", " er",  20, t4);
 qtitle("top view test #2");
-
 
 # close all the windows
 qclear()
